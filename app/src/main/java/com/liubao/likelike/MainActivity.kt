c8f3de436f28aeaba7d3a016c9bf3c4c1dcc80fe
bao.liu <bao.liu@grabtaxi.com>
2022-01-12 19:33:30 +0800

    fix

--- app/src/main/java/com/liubao/likelike/MainActivity.kt
+++ app/src/main/java/com/liubao/likelike/MainActivity.kt
@@ -1,11 +1,92 @@
 package com.liubao.likelike
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Point
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.graphics.get
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    val img1 by lazy {
+        findViewById<ImageView>(R.id.img1)
+    }
+    val img2 by lazy {
+        findViewById<ImageView>(R.id.img2)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val screenShot = BitmapFactory.decodeResource(resources, R.drawable.screen_shot)
+//        val screenShotClip = Bitmap.createBitmap(screenShot, 0, 11, screenShot.width, 120)
+        val screenShotClip = BitmapFactory.decodeResource(resources, R.drawable.screen_shot_clip2)
+        val value1 = screenShot.getPixel(0, 0)
+        val value2 = screenShotClip.getPixel(0, 0)
+        Log.d(TAG, (value1 == value2).toString())
+
+        img1.setImageDrawable(ColorDrawable(value1))
+        img2.setImageDrawable(ColorDrawable(value2))
+
+
+        isContains(screenShot, screenShotClip).also {
+            Log.d(TAG, it.toString())
+
+        }
+        findViewById<TextView>(R.id.btn).setOnClickListener {
+            val point = isContains(screenShot, screenShotClip).toString()
+            Log.d(TAG, point.toString())
+        }
+    }
+
+    private fun isContains(screenShot: Bitmap?, screenShotClip: Bitmap?): Point {
+        if (screenShot == null || screenShotClip == null) return Point(-1, -1)
+        Log.d(TAG, (screenShot.getPixel(0, 0) == screenShotClip.getPixel(0, 0)).toString())
+
+
+
+
+        for (y in 0 until screenShot.height - screenShotClip.height) {
+            if (screenShot.width == screenShotClip.width) {
+                val find = ifAllEqual(
+                    screenShot, screenShotClip, 0, y
+                )
+                if (find) return Point(0, y)
+            } else {
+                for (x in 0 until screenShot.width - screenShotClip.width) {
+                    val find = ifAllEqual(
+                        screenShot, screenShotClip, x, y
+                    )
+                    if (find) return Point(x, y)
+                }
+            }
+        }
+
+        return Point(-1, -1)
+
+    }
+
+    private fun ifAllEqual(bitmap1: Bitmap, bitmap2: Bitmap, startX: Int, startY: Int): Boolean {
+        for (y in 0 until bitmap2.height) {
+            for (x in 0 until bitmap2.width) {
+                val color2 = bitmap2.getPixel(x, y)
+                val color1 = bitmap1.getPixel(startX + x, startY + y)
+                if (color1 != color2) {
+                    return false
+                }
+            }
+        }
+        return true
     }
 }
+
--- app/src/main/java/com/liubao/likelike/MyAccessibility.kt
+++ app/src/main/java/com/liubao/likelike/MyAccessibility.kt
@@ -1,8 +1,11 @@
 package com.liubao.likelike
 
 import android.accessibilityservice.AccessibilityService
+import android.graphics.Rect
+import android.os.Handler
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
 
 class MyAccessibility : AccessibilityService() {
     companion object {
@@ -16,13 +19,18 @@ class MyAccessibility : AccessibilityService() {
         var eventText = "";
         Log.i(TAG, "==============Start====================");
         when (eventType) {
-            AccessibilityEvent.TYPE_VIEW_CLICKED ->
+            AccessibilityEvent.TYPE_VIEW_CLICKED -> {
                 eventText = "TYPE_VIEW_CLICKED";
+            }
 
             AccessibilityEvent.TYPE_VIEW_LONG_CLICKED -> {
                 eventText = "TYPE_VIEW_LONG_CLICKED"
-                if (event.contentDescription == "Comment")
-                    onCommentClicked(event)
+
+
+            }
+            AccessibilityEvent.TYPE_VIEW_FOCUSED -> {
+                eventText = "TYPE_VIEW_FOCUSED"
+                onCommentShow(event)
 
             }
 
@@ -34,19 +42,88 @@ class MyAccessibility : AccessibilityService() {
 
             AccessibilityEvent.CONTENT_CHANGE_TYPE_SUBTREE ->
                 eventText = "CONTENT_CHANGE_TYPE_SUBTREE"
+            AccessibilityEvent.TYPE_VIEW_SCROLLED -> {
+                onWindowContentChanged(event)
+                eventText = "TYPE_WINDOW_CONTENT_CHANGED"
 
+            }
         }
+
         Log.i(TAG, eventType.toString())
         Log.i(TAG, eventText)
-
 
         Log.i(TAG, "=============END=====================");
     }
 
-    private fun onCommentClicked(event: AccessibilityEvent) {
-
-
+    private fun onCommentShow(event: AccessibilityEvent) {
+        if (event.source == null) return
+        Log.i(TAG, event.source.toString())
+        if (event.source.className.equals("android.widget.LinearLayout") == false) return
+        if (event.source.childCount != 2) return
+        event.source.findAccessibilityNodeInfosByText("Like")?.apply {
+            this.forEach {
+                var result = it.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                result = it.parent?.performAction(AccessibilityNodeInfo.ACTION_CLICK) ?: result
+                Log.d(TAG, result.toString())
+            }
+        }
+        findNodeByFilter(event.source, "Like", "Like")?.apply {
+            this.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+        }
     }
+
+    val mainHandler = Handler()
+
+    val hashMap: HashMap<AccessibilityNodeInfo, Boolean> = HashMap()
+    var oldNode: AccessibilityNodeInfo? = null
+    var oldTime = 0L
+    var bounds = Rect()
+    var filterBounds = Rect(0, 400, 8888, 1600)
+    private fun onWindowContentChanged(event: AccessibilityEvent) {
+        val targetItem = event.source.getChild(1)
+        val count = targetItem.childCount
+
+        val likeNode = findNodeByFilter(targetItem, "Comment")
+        if (likeNode == null) return
+        likeNode.getBoundsInScreen(bounds)
+//        if (!filterBounds.contains(bounds)) {
+//            return
+//        }
+//        if (likeNode == oldNode) return
+        oldNode = likeNode
+        Log.d(TAG, "like node is ${likeNode?.toString()}")
+        likeNode?.apply {
+            val timeToPost = if (System.currentTimeMillis() - oldTime < 1000) {
+                100
+            } else {
+                100
+            }
+            oldTime = System.currentTimeMillis()
+            val performed = likeNode?.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+            Log.d(TAG, "node click performed is ${performed}")
+        }
+    }
+
+    private fun findNodeByFilter(
+        node: AccessibilityNodeInfo,
+        contentDescription: String? = null,
+        text: String? = null
+    ): AccessibilityNodeInfo? {
+        val targetText = if (contentDescription.isNullOrEmpty()) text else contentDescription
+        var likeNode: AccessibilityNodeInfo? = null
+        val count = node.childCount
+        for (index in 0 until count) {
+            node.getChild(index).apply {
+                if (targetText == this.contentDescription || targetText == this.text) {
+                    likeNode = this
+                } else {
+                    likeNode = findNodeByFilter(this, contentDescription, text)
+                }
+            }
+        }
+        return likeNode
+    }
+
 
     override fun onInterrupt() {
 
